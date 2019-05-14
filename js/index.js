10892a3706a96a5fe0b1af8974cc674cf5de5193
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-14 13:37:23 +0100

    Add Mouseover event on nav menus

--- js/index.js
+++ js/index.js
@@ -1,22 +1,32 @@
-// Your code goes here
-document.addEventListener(mouseover, e => {});
+// Add Mouseover event on nav menus
+const navItems = document.querySelectorAll('nav a');
+navItems.forEach(item =>
+  item.addEventListener('mouseover', e => {
+    e.target.classList.toggle('hovered');
+  })
+);
+navItems.forEach(item =>
+  item.addEventListener('mouseout', e => {
+    e.target.classList.toggle('hovered');
+  })
+);
 
-document.addEventListener(keydown, e => {});
+document.addEventListener('keydown', e => {});
 
-document.addEventListener(wheel, e => {});
+document.addEventListener('wheel', e => {});
 
-document.addEventListener(drag, e => {});
+document.addEventListener('drag', e => {});
 
-document.addEventListener(drop, e => {});
+document.addEventListener('drop', e => {});
 
-document.addEventListener(load, e => {});
+document.addEventListener('load', e => {});
 
-document.addEventListener(focus, e => {});
+document.addEventListener('focus', e => {});
 
-document.addEventListener(resize, e => {});
+document.addEventListener('resize', e => {});
 
-document.addEventListener(scroll, e => {});
+document.addEventListener('scroll', e => {});
 
-document.addEventListener(select, e => {});
+document.addEventListener('select', e => {});
 
-document.addEventListener(dblclick, e => {});
+document.addEventListener('dblclick', e => {});
