49104c110d0e9d44b71d6d2ad7835196baba10ef
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-14 13:15:39 +0100

    Challenge kickstart

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+document.addEventListener(mouseover, e => {});
+
+document.addEventListener(keydown, e => {});
+
+document.addEventListener(wheel, e => {});
+
+document.addEventListener(drag, e => {});
+
+document.addEventListener(drop, e => {});
+
+document.addEventListener(load, e => {});
+
+document.addEventListener(focus, e => {});
+
+document.addEventListener(resize, e => {});
+
+document.addEventListener(scroll, e => {});
+
+document.addEventListener(select, e => {});
+
+document.addEventListener(dblclick, e => {});
